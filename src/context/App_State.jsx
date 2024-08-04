7b3486d6171f2beecaebd0e9ceca1d7bedc61d26
Shivyansh05 <128629467+Shivyansh05@users.noreply.github.com>
2024-08-05 01:22:38 +0530

    Update App_State.jsx

--- src/context/App_State.jsx
+++ src/context/App_State.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { AppContext } from "./App_Context";
 import axios from "axios";
 const App_State = (props) => {
-  const url = "http://localhost:3000/api";
+  const url = "https://food-recipe-sharing-mern-webapp.onrender.com/api";
   const [token, setToken] = useState("");
   const [recipe, setrecipe] = useState([]);
   const [savedRecipe, setsavedRecipe] = useState([]);
