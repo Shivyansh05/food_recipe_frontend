ea7ebf5c6e3ed32fcc3675b26e49be8f9d2331b4
Shivyansh05 <128629467+Shivyansh05@users.noreply.github.com>
2024-08-05 03:05:56 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
   const navigate= useNavigate();
   const handledelete = async(id)=>{
    const del = await deleterecipe(id);
-   location.reload(false)
+   
   }
   return (
     <>
